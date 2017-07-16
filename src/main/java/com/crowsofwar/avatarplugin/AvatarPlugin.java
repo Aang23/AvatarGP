126969b78993dd113fd523878c645834aac8aff1
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-16 14:34:56 -0500

    Avatar Plugin??

--- src/main/java/com/crowsofwar/avatarplugin/AvatarPlugin.java
+++
@@ -1,9 +0,0 @@
-package com.crowsofwar.avatarplugin;
-
-import org.spongepowered.api.plugin.Plugin;
-
-@Plugin(id = "avatarplugin", name = "AvatarMod2 GriefPrevention Integration Plugin", version = "1.0")
-public class AvatarPlugin {
-
-
-}
